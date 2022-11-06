5b82980ea3022d5f798485e3b96726111e90e2f6
Cameron Abel <cameronabel@gmail.com>
2022-11-06 01:01:44 -0800

    add q3 and support for q4

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,22 @@ function goToQ2 () {
   newText.classList.add('loaded');
 }
 
+function goToQ3 () {
+  const oldText = document.getElementById("q2-text");
+  const oldImage = document.getElementById("q2-img");
+  const newImage = document.getElementById("q3-img");
+  const newText = document.getElementById("q3-text");
+  
+  oldText.classList.toggle("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.toggle("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
+}
+
 window.onload = function () {
   const titleButton = document.getElementById("start-btn");
   titleButton.addEventListener("click", goToQ1);
@@ -42,4 +58,13 @@ window.onload = function () {
       goToQ2()
     })
   }
+
+  let form2 = document.getElementsByName("exp")
+  for (let platformButton of form2) {
+    platformButton.addEventListener('click', function () {
+      localStorage.platform = platformButton.value
+      goToQ3()
+    })
+  }
+
 }
