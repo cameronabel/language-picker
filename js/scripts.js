23da2eed68d94d9865494a057518f8fc331f588a
Cameron Abel <cameronabel@gmail.com>
2022-11-06 02:08:24 -0800

    add results page

--- js/scripts.js
+++ js/scripts.js
@@ -1,116 +1,158 @@
-function goToQ1 (event) {
+function goToQ1(event) {
   event.preventDefault();
   const oldText = document.getElementById("title");
   const oldImage = document.getElementById("title-image");
   const newImage = document.getElementById("q1-img");
   const newText = document.getElementById("q1-text")
   
-  oldText.classList.remove("loaded")
-  oldText.classList.add("hidden")
-  newImage.classList.add("loaded")
-  newImage.classList.remove("hidden")
-  oldImage.classList.remove("loaded")
-  oldImage.classList.add("hidden")
-  newText.classList.remove('hidden')
-  newText.classList.add('loaded')
+  oldText.classList.remove("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.remove("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
 }
 
-function goToQ2 () {
+function goToQ2() {
   const oldText = document.getElementById("q1-text");
   const oldImage = document.getElementById("q1-img");
   const newImage = document.getElementById("q2-img");
   const newText = document.getElementById("q2-text");
   
-  oldText.classList.remove("loaded")
-  oldText.classList.add("hidden")
-  newImage.classList.add("loaded")
-  newImage.classList.remove("hidden")
-  oldImage.classList.remove("loaded")
-  oldImage.classList.add("hidden")
-  newText.classList.remove('hidden')
-  newText.classList.add('loaded')
+  oldText.classList.remove("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.remove("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
 }
 
-function goToQ3 () {
+function goToQ3() {
   const oldText = document.getElementById("q2-text");
   const oldImage = document.getElementById("q2-img");
   const newImage = document.getElementById("q3-img");
   const newText = document.getElementById("q3-text");
   
-  oldText.classList.remove("loaded")
-  oldText.classList.add("hidden")
-  newImage.classList.add("loaded")
-  newImage.classList.remove("hidden")
-  oldImage.classList.remove("loaded")
-  oldImage.classList.add("hidden")
-  newText.classList.remove('hidden')
-  newText.classList.add('loaded')
+  oldText.classList.remove("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.remove("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
 }
 
-function goToQ4 () {
+function goToQ4() {
   const oldText = document.getElementById("q3-text");
   const oldImage = document.getElementById("q3-img");
   const newImage = document.getElementById("q4-img");
   const newText = document.getElementById("q4-text");
   
-  oldText.classList.remove("loaded")
-  oldText.classList.add("hidden")
-  newImage.classList.add("loaded")
-  newImage.classList.remove("hidden")
-  oldImage.classList.remove("loaded")
-  oldImage.classList.add("hidden")
-  newText.classList.remove('hidden')
-  newText.classList.add('loaded')
+  oldText.classList.remove("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.remove("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
 }
 
-function goToQ5 () {
+function goToQ5() {
   const oldText = document.getElementById("q4-text");
   const oldImage = document.getElementById("q4-img");
   const newImage = document.getElementById("q5-img");
   const newText = document.getElementById("q5-text");
   
-  oldText.classList.remove("loaded")
-  oldText.classList.add("hidden")
-  newImage.classList.add("loaded")
-  newImage.classList.remove("hidden")
-  oldImage.classList.remove("loaded")
-  oldImage.classList.add("hidden")
-  newText.classList.remove('hidden')
-  newText.classList.add('loaded')
+  oldText.classList.remove("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.remove("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
 }
 
-function goToResult () {
+function goToResult() {
   const oldText = document.getElementById("q5-text");
   const oldImage = document.getElementById("q5-img");
   const leftDiv = document.getElementById("left-half");
   const rightDiv = document.getElementById("right-half");
 
   
-  oldText.classList.remove("loaded")
-  oldText.classList.add("hidden")
-  oldImage.classList.remove("loaded")
-  oldImage.classList.add("hidden")
-  leftDiv.classList.remove("loaded")
-  leftDiv.classList.add("hidden")
-  rightDiv.classList.remove("loaded")
-  rightDiv.classList.add("hidden")
+  oldText.classList.remove("loaded");
+  oldText.classList.add("hidden");
+  oldImage.classList.remove("loaded");
+  oldImage.classList.add("hidden");
+  leftDiv.classList.remove("loaded");
+  leftDiv.classList.add("hidden");
+  rightDiv.classList.remove("loaded");
+  rightDiv.classList.add("hidden");
   displayResult()
 }
 
 function displayResult() {
-  const platform = localStorage.platform
-  console.log(platform)
-}
+  const platform = localStorage.platform;
+  const exp = localStorage.exp;
+  const legacy = localStorage.legacy;
+  const game = localStorage.game;
+  const popular = localStorage.popular;
 
+  let result;
+  if (popular === 'obscure') {
+    result = document.getElementById("brainfuck");
+  } else if (exp === 'no') {
+    result = document.getElementById("python");
+  } else if (legacy === 'old') {
+    result = document.getElementById("cobol");
+  } else if (platform === 'mobile') {
+    result = document.getElementById("swift");
+  } else if (platform === 'web' && game === 'gamer') {
+    result = document.getElementById("javascript");
+  } else if (platform === 'web' && game === 'non-gamer') {
+    result = document.getElementById("ruby");
+  } else if (platform === 'desktop' && game === 'gamer'){
+    result = document.getElementById("c/c#");
+  }
+  const resultField = document.getElementById("results")
+  resultField.classList.remove('hidden')
+  result.classList.remove('hidden')
+  const restartButton = document.getElementById("start-over-btn");
+  restartButton.addEventListener("click", function () {
+    resultField.classList.add('hidden')
+    result.classList.add('hidden');
+    const leftDiv = document.getElementById("left-half");
+    const rightDiv = document.getElementById("right-half");
+    leftDiv.classList.remove("hidden");
+    leftDiv.classList.add("loaded");
+    rightDiv.classList.remove("hidden");
+    rightDiv.classList.add("loaded");
+
+    const newText = document.getElementById("title");
+    const newImage = document.getElementById("title-image");
+    newText.classList.remove('hidden');
+    newText.classList.add('loaded');
+    newImage.classList.add("loaded");
+    newImage.classList.remove("hidden");
+  }
+  );
+}
 
 window.onload = function () {
   const titleButton = document.getElementById("start-btn");
   titleButton.addEventListener("click", goToQ1);
 
+
   let form1 = document.getElementsByName("platform")
   for (let selection of form1) {
     selection.addEventListener('click', function () {
-      localStorage.platform = selection.value
+      localStorage.platform = selection.value;
       goToQ2()
     })
   }
@@ -118,7 +160,7 @@ window.onload = function () {
   let form2 = document.getElementsByName("exp")
   for (let selection of form2) {
     selection.addEventListener('click', function () {
-      localStorage.exp = selection.value
+      localStorage.exp = selection.value;
       goToQ3()
     })
   }
