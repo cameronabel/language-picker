fbe38c3fe3b6a66eb0999f7920a572d8b88c0adc
Cameron Abel <cameronabel@gmail.com>
2022-11-06 01:45:50 -0700

    added q2 and support to move to q3

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,45 @@
 function goToQ1 (event) {
   event.preventDefault();
-  oldText = document.getElementById("title");
-  qImage = document.getElementById("q1-img");
+  const oldText = document.getElementById("title");
+  const oldImage = document.getElementById("title-image");
+  const newImage = document.getElementById("q1-img");
+  const newText = document.getElementById("q1-text")
+  
   oldText.classList.toggle("loaded")
   oldText.classList.add("hidden")
-  qImage.classList.add("loaded")
-  qImage.classList.remove("hidden")
-  oldImage = document.getElementById("title-image")
+  newImage.classList.add("loaded")
+  newImage.classList.remove("hidden")
   oldImage.classList.toggle("loaded")
   oldImage.classList.add("hidden")
-  newText = document.getElementById("q1-text")
   newText.classList.remove('hidden')
   newText.classList.add('loaded')
+}
+
+function goToQ2 () {
+  const oldText = document.getElementById("q1-text");
+  const oldImage = document.getElementById("q1-img");
+  const newImage = document.getElementById("q2-img");
+  const newText = document.getElementById("q2-text");
+  
+  oldText.classList.toggle("loaded");
+  oldText.classList.add("hidden");
+  newImage.classList.add("loaded");
+  newImage.classList.remove("hidden");
+  oldImage.classList.toggle("loaded");
+  oldImage.classList.add("hidden");
+  newText.classList.remove('hidden');
+  newText.classList.add('loaded');
 }
 
 window.onload = function () {
   const titleButton = document.getElementById("start-btn");
   titleButton.addEventListener("click", goToQ1);
+
+  let form1 = document.getElementsByName("platform")
+  for (let platformButton of form1) {
+    platformButton.addEventListener('click', function () {
+      localStorage.platform = platformButton.value
+      goToQ2()
+    })
+  }
 }
